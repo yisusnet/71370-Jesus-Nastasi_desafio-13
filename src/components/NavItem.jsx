86f2c170ec4914a04fa7129806bcbbadd6f9423d
jesus nastasi <jesusnastasi@gmail.com>
2024-09-19 02:42:41 -0300

    desestruracion de la props

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,31 +1,13 @@
 import { NavLink } from "react-router-dom";
 
 const NavItem = ({ objItem }) => {
-  const { nombre, ruta } = objItem
+ 
 
   return (
     <li className="nav-item">
-      <NavLink className="nav-link" to={ruta}>{nombre}</NavLink>
+      <NavLink className="nav-link" to={objItem}>{objItem.nombre}</NavLink>
     </li>
   );
 }
 
 export default NavItem;
-/*     <>
-    
-    <li className="nav-item">
-    <NavLink className="nav-link" to="/" >Inicio </NavLink>
-  </li>
-
-  <li className="nav-item">
-    <NavLink className="nav-link" to="/usuarios">Usuarios</NavLink>
-  </li>
-
-  <li className="nav-item">
-    <NavLink className="nav-link" to="/nosotros">Nosotros</NavLink>
-  </li>
-
-  <li className="nav-item">
-    <NavLink className="nav-link" to="/contacto">Contacto</NavLink>
-  </li>
-  </> */

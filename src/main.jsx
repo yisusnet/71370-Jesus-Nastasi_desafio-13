ce8bf7227f9544d7d902c484cd891ebb98fd7dac
jesus nastasi <jesusnastasi@gmail.com>
2024-09-18 22:41:41 -0300

    instalo react router y modifico el main.jsx para crear el browserrouter, routes, route,

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from 'react-dom/client'
 import InicioApp from './InicioApp'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Swal from 'sweetalert2'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoEncontrado from '../pages/NoEncontrado';
+import Contacto from '../pages/Contacto';
+import Nosotros from '../pages/Nosotros';
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<InicioApp/>
+    <BrowserRouter>
+    <Routes>   
+      
+  <Route path='*'  element={<NoEncontrado/>} />
+  <Route path='/'  element={<InicioApp/>} />
+  <Route path='/contacto'  element={<Contacto/>} />
+  <Route path='/nosotros'  element={<Nosotros/>} />
+
+    </Routes>
+    </BrowserRouter>
+
   </StrictMode>,
 )
